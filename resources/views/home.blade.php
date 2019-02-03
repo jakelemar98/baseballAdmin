346ac842a92340c4a59d846a5fc3c83821997585
jakelemar98 <30849493+jakelemar98@users.noreply.github.com>
2019-02-02 22:12:58 -0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                  @foreach($practice as $prac)
-			<h1>{{ $prac->practice_name  }}</h1>
-			<h2>{{ date("g:i a", strtotime("15:00"))  }}</h2>
-		  @endforeach
-                </div>
             </div>
         </div>
     </div>
